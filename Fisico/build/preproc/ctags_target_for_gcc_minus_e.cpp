100d9ba40ab6d59f5199470dc1fa1a19a74e85d0
Gustavo Giron <ggironarriolaa@gmail.com>
2023-06-14 18:40:49 -0700

    Fisico
    Fisico creado

---
+++ Fisico/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,10 @@
+# 1 "C:\\Users\\ggiro\\Desktop\\ACE1-23VJ0778G13PROY1\\Fisico\\Fisico.ino"
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ Fisico/build/sketch/Fisico.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\ggiro\\Desktop\\ACE1-23VJ0778G13PROY1\\Fisico\\Fisico.ino"
+#line 1 "C:\\Users\\ggiro\\Desktop\\ACE1-23VJ0778G13PROY1\\Fisico\\Fisico.ino"
+void setup();
+#line 6 "C:\\Users\\ggiro\\Desktop\\ACE1-23VJ0778G13PROY1\\Fisico\\Fisico.ino"
+void loop();
+#line 1 "C:\\Users\\ggiro\\Desktop\\ACE1-23VJ0778G13PROY1\\Fisico\\Fisico.ino"
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
